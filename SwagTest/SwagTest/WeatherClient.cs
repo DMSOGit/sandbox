365306e3e62e236c04c8971cd394bf102d5f8672
Dom <dmarcilstonge@gmail.com>
2023-01-30 18:42:25 -0500

    Added dependency injection to WeatherClient

--- SwagTest/SwagTest/WeatherClient.cs
+++ SwagTest/SwagTest/WeatherClient.cs
@@ -5,13 +5,18 @@ namespace SwagTest
     public class WeatherClient
     {
         private const string API_URL = "https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&hourly=temperature_2m";
-        private readonly HttpClient client = new HttpClient();
+        private readonly HttpClient _client;
 
+        //Singleton, dont instanciate
+        public WeatherClient(HttpClient client) 
+        {
+            _client = client;
+        }
         public string GetWeatherData(string latitude, string longitude)
         {
             string data;
             string url = string.Format(API_URL, latitude, longitude);
-            HttpResponseMessage response = client.GetAsync(url).Result;
+            HttpResponseMessage response = _client.GetAsync(url).Result;
             if (response.IsSuccessStatusCode)
             {
                 data = response.Content.ReadAsStringAsync().Result;
