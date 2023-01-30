a37fd2c64876d56170dfba5f1ec56f52028bd3f1
Dom <dmarcilstonge@gmail.com>
2023-01-30 14:49:33 -0500

    Adding SwagTest
    A small project to try out making an API using Swagger

---
+++ SwagTest/SwagTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwagTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("[action]")]
+        public string GetWeatherData(string latitude = "45.51", string longitude = "-73.59")
+        {
+            WeatherClient client = WeatherClient.Instance;
+            return client.GetWeatherData(latitude, longitude);
+        }
+    }
+}
